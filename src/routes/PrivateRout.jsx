0cc571ac071fa20eb8cb7a5a8f2fb3ac893ca4f0
Mazharul Islam <mazharul3islam0@gmail.com>
2024-12-09 22:55:42 +0600

    Introduction to Private Route and handle loading state

--- src/routes/PrivateRout.jsx
+++ src/routes/PrivateRout.jsx
@@ -5,7 +5,7 @@ import { Navigate } from "react-router-dom";
 const PrivateRout = ({children}) => {
     const {user, loading} = useContext(AuthContext);
     if(loading){
-        return
+        return <span className="loading loading-spinner loading-lg"></span>
     }
 
     if(user){
